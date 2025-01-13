4113e50df6cd4d92cacc59d765e3b14be4345fc6
Tiodor <tiodor.xgm.ru@gmail.com>
2025-01-13 03:37:56 +0200

    Add project files.

---
+++ DateTimeCalculator/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DateTimeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTimestamp = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTimestamp
+            // 
+            this.labelTimestamp.AutoSize = true;
+            this.labelTimestamp.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimestamp.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelTimestamp.Location = new System.Drawing.Point(8, 7);
+            this.labelTimestamp.Name = "labelTimestamp";
+            this.labelTimestamp.Size = new System.Drawing.Size(31, 13);
+            this.labelTimestamp.TabIndex = 0;
+            this.labelTimestamp.Text = "0000";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.AutoSize = true;
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.BorderThickness = 2;
+            this.guna2Panel1.Controls.Add(this.labelTimestamp);
+            this.guna2Panel1.FillColor = System.Drawing.Color.DeepSkyBlue;
+            this.guna2Panel1.Location = new System.Drawing.Point(512, 12);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(76, 27);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.guna2Panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Dater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTimestamp;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+    }
+}
+
---
+++ DateTimeCalculator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeCalculator
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeTimer()
+        {            
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += OnTimerTick; 
+            timer.Start(); 
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {            
+            long unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();       
+            labelTimestamp.Text = $"{unixTimestamp}";
+        }
+    }
+}
